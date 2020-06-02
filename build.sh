7f6558007aee02d8b2cf227ac20f0d76cddc1bae
Pierre GINDRAUD <pgindraud@gmail.com>
2020-06-03 00:18:32 +0200

    Update typo

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ DOCKERFILE_PATH="Dockerfile"
 set -e
 
 # If empty version, fetch the latest from repository
-if [ -z "$ARA_VERSION" ]; then
-  ARA_VERSION=`curl --fail -s https://api.github.com/repos/ansible-community/ara/tags | grep --perl-regexp --only-matching '(?<=name": ")[0-9.]+' -m 1`
-  if [[ $? -ne 0 ]]; then
+if [[ -z "${ARA_VERSION}" ]]; then
+  if ! ARA_VERSION=$(curl --fail -s https://api.github.com/repos/ansible-community/ara/tags | \
+                     grep --perl-regexp --only-matching '(?<=name": ")[0-9.]+' -m 1); then
     echo 'Error during fetch last ara version'
     exit 1
   fi
@@ -21,37 +21,33 @@ fi
 echo "-> selected ARA version '${ARA_VERSION}'"
 
 # If empty commit, fetch the current from local git rpo
-if [ -n "${SOURCE_COMMIT}" ]; then
+if [[ -n "${SOURCE_COMMIT}" ]]; then
   VCS_REF="${SOURCE_COMMIT}"
-elif [ -n "${TRAVIS_COMMIT}" ]; then
+elif [[ -n "${TRAVIS_COMMIT}" ]]; then
   VCS_REF="${TRAVIS_COMMIT}"
 else
-  VCS_REF="`git rev-parse --short HEAD`"
+  VCS_REF="$(git rev-parse --short HEAD)"
 fi
 test -n "${VCS_REF}"
 echo "-> current vcs reference '${VCS_REF}'"
 
 # Get the current image static version
-image_version=`cat VERSION`
+image_version=$(cat VERSION)
 echo "-> use image version '${image_version}'"
 
 # Compute variant from dockerfile name
-if ! [ -f ${DOCKERFILE_PATH} ]; then
+if ! [[ -f ${DOCKERFILE_PATH} ]]; then
   echo 'You must select a valid dockerfile with DOCKERFILE_PATH' 1>&2
   exit 1
 fi
-if [[ -n "${IMAGE_VARIANT}" ]]; then
-  image_building_name="${DOCKER_IMAGE}:building_${IMAGE_VARIANT}"
-  echo "-> set image variant '${IMAGE_VARIANT}' for build"
-else
-  image_building_name="${DOCKER_IMAGE}:building"
-fi
+
+image_building_name="${DOCKER_IMAGE}:building"
 echo "-> use image name '${image_building_name}' for build"
 
 ## Build image
 echo "=> building '${image_building_name}' with image version '${image_version}'"
 docker build --build-arg "ARA_VERSION=${ARA_VERSION}" \
-             --label "org.label-schema.build-date=`date -u +'%Y-%m-%dT%H:%M:%SZ'`" \
+             --label "org.label-schema.build-date=$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
              --label 'org.label-schema.name=ansible-araserver' \
              --label 'org.label-schema.description=ARA API web application' \
              --label 'org.label-schema.url=https://github.com/Turgon37/docker-ansible-araserver' \
